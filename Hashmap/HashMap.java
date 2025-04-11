ddda768090678c107253b517b8457371e9fa0eca
PrateekSinha2022 <sinhaprateek2020@gmail.com>
2025-04-11 10:02:20 +0530

    12th commit

---
+++ Hashmap/HashMap.java
@@ -0,0 +1,98 @@
+package Hashmap;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class HashMap {
+	
+	private LinkedList<Node>[]  buckets;//Always use the generic<Node>
+	private int n;//no of nodes
+	private int N;//no of buckets
+	
+	public HashMap()
+	{
+		N=4;
+		buckets=new LinkedList[N];
+		for(int i=0;i<N;i++)
+		{
+			buckets[i]=new LinkedList();
+		}
+	}
+	
+	public void put(int key,String value)
+	{
+		int bi = hashFunction(key);
+		int di  = getDataIndex(bi,key);
+		
+		if(bi!=-1)
+		{
+			buckets[bi].get(di).value=value;
+		}
+		else
+		{
+			buckets[bi].add(new Node(key,value));
+			n++;
+		}
+		
+		double lambda = (double) n/N;
+		
+		if(lambda>2.0)
+		{
+			rehash();
+		}
+	}
+	
+	private void rehash()
+	{
+		LinkedList<Node>[] old=buckets;
+		buckets = new LinkedList[N*2];
+		N=buckets.length;
+		n=0;
+		
+		for(int i=0;i<old.length;i++)
+		{
+			LinkedList<Node> list=old[i];
+			for(int j=0;j<list.size();j++)
+			{
+				Node node=list.get(j);
+				put(node.key,node.value);
+			}
+		}
+	}
+	
+	private int hashFunction(int key)
+	{
+		int hashcode=Objects.hash(key);
+		hashcode=Math.abs(hashcode);
+		return hashcode%buckets.length;
+		
+	}
+	
+	private int getDataIndex(int bi,int key)
+
+	{
+		LinkedList<Node> list=buckets[bi];
+		for(int i=0;i<list.size();i++)
+		{
+			Node node=list.get(i);
+			if(node.key==key)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private class Node
+	{
+		int key;
+		String value;
+		Node(int key,String value)
+
+		{
+			this.key=key;
+			this.value=value;
+		}
+	}
+
+}
---
+++ Hashmap/SinglyLinkedListDemo.java
@@ -0,0 +1,15 @@
+package Hashmap;
+
+import java.util.LinkedList;
+
+public class SinglyLinkedListDemo {
+	public static void main(String[] args) {
+		
+		LinkedList list=new LinkedList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+	System.out.println(list);
+	}
+
+}
